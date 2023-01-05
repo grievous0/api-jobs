f083c28329129a51e49cdd757525515b71e0ebf3
João Vitor Oliveira Pereira <joao.pereira@ibm.com>
2023-01-05 18:58:53 -0300

    Database configuration

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,23 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 import express from 'express'
+import { Candidate } from './models/candidates';
 
 const app = express()
 
 const router = express.Router()
 
-router.get('/', (req, res) => 
+router.get('/', (req, res) => {
     res.json({ 
         message: 'Hello World!'
     })
-)
+})
+
+router.get('/candidates', async (req, res) => {
+    const candidates = await Candidate.findAll()
+
+    return res.status(200).json(candidates)
+})
 
 app.use(router)
 
