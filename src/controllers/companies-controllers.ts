cfa896eb34513a0466914a067de1163abbbb4ed9
grievous0 <jvopereira2004@gmail.com>
2023-01-09 12:17:53 -0300

    Add companies endpoints

--- src/controllers/companies-controllers.ts
+++ src/controllers/companies-controllers.ts
@@ -13,5 +13,87 @@ export const companiesController = {
                 })
             }
         }
-    }
+    },
+
+    // POST /companies
+    save: async (req: Request, res: Response) => {
+        const { name, bio, website, email } = req.body;
+
+        try {
+            const companies = await Company.create({
+                name,
+                bio,
+                website,
+                email
+            })
+            res.status(201).json(companies);
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({
+                    error: error.message
+                })
+            }
+        }
+    },
+
+    // GET /companies/:id
+    show: async (req: Request, res: Response) => {
+        const { id } = req.params;
+
+        try {
+            const company = await Company.findByPk(id);
+            res.status(200).json(company);
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({
+                    error: error.message
+                })
+            }
+        }
+    },
+
+    // PUT /companies/:id
+    update: async (req: Request, res: Response) => {
+        const { id } = req.params;
+        const { name, bio, website, email } = req.body;
+        
+        try {
+            const [affectedRows, companies] = await Company.update({
+                name,
+                bio,
+                website,
+                email
+            }, { 
+                where: { id },
+                returning: true
+            })
+
+            return res.status(200).json(companies[0]);
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({
+                    error: error.message
+                })
+            }
+        }
+    },
+
+    // DELETE /companies/:id
+    delete: async (req: Request, res: Response) => {
+        const { id } = req.params;
+        
+        try {
+            await Company.destroy({ where: { id } })
+
+            return res.status(200).json({
+                message: 'Company deleted successfully'
+            });
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({
+                    error: error.message
+                })
+            }
+        }
+    },
 }
--- src/routes.ts
+++ src/routes.ts
@@ -5,12 +5,18 @@ import { companiesController } from './controllers/companies-controllers'
 
 const router = express.Router()
 
+// Candidates
 router.get('/candidates', candidatesController.index)
 router.get('/candidates/:id', candidatesController.show)
 router.post('/candidates', candidatesController.save)
 router.put('/candidates/:id', candidatesController.update)
 router.delete('/candidates/:id', candidatesController.delete)
 
+// Companies
 router.get('/companies', companiesController.index)
+router.get('/companies/:id', companiesController.show)
+router.post('/companies', companiesController.save)
+router.put('/companies/:id', companiesController.update)
+router.delete('/companies/:id', companiesController.delete)
 
 export { router }
